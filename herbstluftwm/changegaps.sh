f2b7cc1e75720e813bb1a24a4f38dd907d9ccdfb
allys7 <allys7@protonmail.com>
2022-02-16 21:54:25 -0700

    updated herbstluftwm config + changegaps script

--- herbstluftwm/changegaps.sh
+++ herbstluftwm/changegaps.sh
@@ -8,23 +8,38 @@ case $1 in
     case $2 in
     f | frame)
         herbstclient set_attr settings.frame_gap $(($(herbstclient get_attr settings.frame_gap) + $3))
-        notify-send "increased frame gaps by $3"
+        notify-send "increased frame gaps by $3  ($(herbstclient get_attr settings.frame_gap))" -t 200
         ;;
     w | window)
         herbstclient set_attr settings.window_gap $(($(herbstclient get_attr settings.window_gap) + $3))
-        notify-send "increased window gaps by $3"
+        notify-send "increased window gaps by $3  ($(herbstclient get_attr settings.window_gap))" -t 200
         ;;
     esac
     ;;
 - | minus | subtract | decrease)
+    current_frame_gap=$(herbstclient get_attr settings.frame_gap)
+    current_window_gap=$(herbstclient get_attr settings.window_gap)
+    echo $current_frame_gap $current_window_gap
     case $2 in
     f | frame)
-        herbstclient set_attr settings.frame_gap $(($(herbstclient get_attr settings.frame_gap) - $3))
-        notify-send "decreased frame gaps by $3"
+        if [ $current_frame_gap -lt $3 ]
+        then 
+            herbstclient set_attr settings.window_gap 0
+            notify-send "decreased frame gap by $current_frame_gap (at 0)" -t 200
+        else
+            herbstclient set_attr settings.frame_gap $(($(herbstclient get_attr settings.frame_gap) - $3))
+            notify-send "decreased frame gap by $3 ($(herbstclient get_attr settings.frame_gap))" -t 200
+        fi
         ;;
     w | window)
-        herbstclient set_attr settings.window_gap $(($(herbstclient get_attr settings.window_gap) - $3))
-        notify-send "decreased window gaps by $3"
+        if [ $current_window_gap -lt $3 ]
+        then
+            herbstclient set_attr settings.window_gap 0
+            notify-send "decreased window gap by $current_window_gap (at 0)"
+        else
+            herbstclient set_attr settings.window_gap $(($(herbstclient get_attr settings.window_gap) - $3))
+            notify-send "decreased window gaps by $3 ($(herbstclient get_attr settings.window_gap))"
+        fi
         ;;
     esac
     ;;
